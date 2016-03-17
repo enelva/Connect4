5fe222fce1b29a4faabc2d02a68b6470d10fa545
Jon Skärlina <pinkrope@hotmail.com>
2016-03-17 13:00:57 +0100

    Lite kommentarer i Form1.cs

--- Connect4/Form1.cs
+++ Connect4/Form1.cs
@@ -18,9 +18,33 @@ namespace Connect4
         public string player2Name = "Jon";
         public const int moduluscheck = 2;
         public int rounds = 0;
+
         public Form1()
         {
             InitializeComponent();
+            //CheckForPreviousClick();
+        }
+
+        private void CheckForPreviousClick(PictureBox thisPictureBox)  //"Ritar" spelplanen och kontrollerar om knappen är tryckt sedan tidigare 
+        {
+            //Koppla varje pictureBox till en koordinat
+            //Kontrollera koordinat om den är ledig dvs tom bricka Connect4Empty.png
+            //OM pictureBox är Empty så fyll den med spelarens färg
+            //ANNARS händer inget
+
+            for (int x = 0; x < 7; x++)
+            {
+                for (int y = 0; y < 6; y++)
+                {
+                    
+                }
+            }
+
+            if (playingField)
+            {
+
+            }
+
         }
 
         private void pictureBox1_Click(object sender, EventArgs e)
@@ -32,13 +56,17 @@ namespace Connect4
         {
             if (rounds % moduluscheck == 0)
             {
-                thisPictureBox.Image = Image.FromFile(@"C:\Users\Administrator\Documents\visual studio 2015\Projects\Connect4\Connect4\Images\Connect4" + player1Color + ".png");
+                thisPictureBox.Image = Image.FromFile(@"C:\Users\Administrator\Source\Repos\Connect4\Connect4\Images\Connect4" + player1Color + ".png");
+                //"C:\Users\Administrator\Documents\visual studio 2015\Projects\Connect4\Connect4\Images\Connect4"  DENNA LÄNK TILL MIKAELS DATOR
+                //"C:\Users\Administrator\Source\Repos\Connect4\Connect4\Images\Connect4"   DENNA LÄNK TILL JONS DATOR
             }
             else
             {
-                thisPictureBox.Image = Image.FromFile(@"C:\Users\Administrator\Documents\visual studio 2015\Projects\Connect4\Connect4\Images\Connect4" + player2Color + ".png");
+                thisPictureBox.Image = Image.FromFile(@"C:\Users\Administrator\Source\Repos\Connect4\Connect4\Images\Connect4" + player2Color + ".png");
             }
             rounds++;
         }
+
+
     }
 }
