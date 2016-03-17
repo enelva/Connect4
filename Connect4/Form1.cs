67d72d755c45f4ea6f52c73aaaacc202c816abcd
Mikael Brunnberg <mikael.brunnberg@gmail.com>
2016-03-17 15:49:16 +0100

    Nu kan man spela

--- Connect4/Form1.cs
+++ Connect4/Form1.cs
@@ -12,62 +12,108 @@ namespace Connect4
 {
     public partial class Form1 : Form
     {
-        public string player1Color = "Red";
-        public string player2Color = "Yellow";
+        public string player1Color_Red = "Red";
+        public string player2Color_Yellow = "Yellow";
         public string player1Name = "Mikael";
         public string player2Name = "Jon";
         public const int moduluscheck = 2;
         public int rounds = 0;
+        public Slot[,] board;
+        public const int boardXWidth = 7;
+        public const int boardYHeight = 6;
 
         public Form1()
         {
             InitializeComponent();
             //CheckForPreviousClick();
-        }
 
-        private void CheckForPreviousClick(PictureBox thisPictureBox)  //"Ritar" spelplanen och kontrollerar om knappen är tryckt sedan tidigare 
-        {
-            //Koppla varje pictureBox till en koordinat
-            //Kontrollera koordinat om den är ledig dvs tom bricka Connect4Empty.png
-            //OM pictureBox är Empty så fyll den med spelarens färg
-            //ANNARS händer inget
+            board = new Slot[boardXWidth, boardYHeight]; // X, Y
 
-            for (int x = 0; x < 7; x++)
+            for (int y = 0; y < boardYHeight; y++) // Går igenom alla rader (Y), uppifrån och ner...
             {
-                for (int y = 0; y < 6; y++)
+                for (int x = 0; x < boardXWidth; x++) // GÅr igenom alla celler i raedrna (X), från höger till vänster...
                 {
-                    
+                    Slot slot = new Slot("Empty");
+                    board[x, y] = slot;
                 }
             }
-
-            if (playingField)
-            {
-
-            }
-
         }
+
 
         private void pictureBox_Click(object sender, EventArgs e)
         {
-            ChangeColor(this.pictureBox1, player1Color);
+            PictureBox p = (PictureBox)sender;
+            string tmpName = p.Name.ToString();
+            string[] tmpNameArray = tmpName.Split('x');
+            int xCoordinate = Convert.ToInt32(tmpNameArray[1]);
+            int yCoordinate = Convert.ToInt32(tmpNameArray[2]);
+
+            if (CheckIfSomethingBelow(xCoordinate, yCoordinate) == true)
+            {
+                ChangeColor(p, xCoordinate, yCoordinate);
+
+                if (CheckIfSomeOneWon() == true)
+                {
+
+                }
+            }
+
+            
         }
 
-        private void pictureBox1_Click(object sender, EventArgs e)
+        private bool CheckIfSomeOneWon()
         {
-            ChangeColor(this.pictureBox1, player1Color);
+            bool someOneWon = false;
+
+            //for (int i = 0; i < length; i++)
+            //{
+            //    boar
+            //}
+
+            return someOneWon;
         }
 
-        private void ChangeColor(PictureBox thisPictureBox, string color)
+        private bool CheckIfSomethingBelow(int xCoordinate, int yCoordinate)
+        {
+            bool isTrue = false;
+
+            if (board[xCoordinate, yCoordinate].color == "Empty")
+            {
+                if (yCoordinate == 0)
+                {
+                    isTrue = true;
+                }
+                else if (board[xCoordinate, (yCoordinate - 1)].color != "Empty")
+                {
+                    isTrue = true;
+                }
+                else
+                {
+                    MessageBox.Show("Ogiltigt drag, försök igen");
+                }
+            }
+            return isTrue;
+        }
+
+        private void ChangeColor(PictureBox thisPictureBox, int xCord, int yCord)
         {
             if (rounds % moduluscheck == 0)
             {
-                thisPictureBox.Image = Image.FromFile(@"C:\Users\Administrator\Source\Repos\Connect4\Connect4\Images\Connect4" + player1Color + ".png");
-                //"C:\Users\Administrator\Documents\visual studio 2015\Projects\Connect4\Connect4\Images\Connect4"  DENNA LÄNK TILL MIKAELS DATOR
-                //"C:\Users\Administrator\Source\Repos\Connect4\Connect4\Images\Connect4"   DENNA LÄNK TILL JONS DATOR
+                //// JONS DATOR
+                //thisPictureBox.Image = Image.FromFile(@"C:\Users\Administrator\Source\Repos\Connect4\Connect4\Images\Connect4" + player1Color + ".png");
+
+                //MIKAELS DATOR
+                thisPictureBox.Image = Image.FromFile(@"C:\Users\Administrator\Documents\visual studio 2015\Projects\Connect4\Connect4\Images\Connect4" + player1Color_Red + ".png");
+                board[xCord, yCord].color = player1Color_Red;
             }
             else
             {
-                thisPictureBox.Image = Image.FromFile(@"C:\Users\Administrator\Source\Repos\Connect4\Connect4\Images\Connect4" + player2Color + ".png");
+                //// JONS DATOR
+                //thisPictureBox.Image = Image.FromFile(@"C:\Users\Administrator\Source\Repos\Connect4\Connect4\Images\Connect4" + player2Color + ".png");
+
+                // MIKAELS DATOR
+                thisPictureBox.Image = Image.FromFile(@"C:\Users\Administrator\Documents\visual studio 2015\Projects\Connect4\Connect4\Images\Connect4" + player2Color_Yellow + ".png");
+                board[xCord, yCord].color = player2Color_Yellow;
             }
             rounds++;
         }
---
+++ Connect4/Slot.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Connect4
+{
+    public class Slot
+    {
+        public Slot(/*int XBoard, int YBoard, */ string Color)
+        {
+            //xBoard = XBoard;
+            //yBoard = YBoard;
+            color = Color;
+        }
+        //public int xBoard { get; set; }
+        //public int yBoard { get; set; }
+        public string color { get; set; }
+
+
+
+    }
+}
